58941947c8e11a065d7670ee788cd604b9941daa
Fernando Ellery <fernando.hle@gmail.com>
2024-07-12 12:29:01 -0300

    primeiro componente

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './componentes/Header/index.js'
+import Header from './componentes/Header/index.js';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
