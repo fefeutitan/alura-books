dfc287b10a7f0b02db6365fe4854e3255ae25a68
Fernando Ellery <fernando.hle@gmail.com>
2024-07-12 12:45:40 -0300

    Criando Containers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
 import './componentes/Header/index.js'
 import Header from './componentes/Header/index.js';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-image: linear-gradient(90deg,#002f52 35%,#326589 100%);
+
+.li {
+    list-style: none;
+}
+`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Header/>
-    </div>
+    </AppContainer>
   );
 }
 
