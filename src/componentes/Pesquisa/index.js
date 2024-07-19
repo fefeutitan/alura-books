f58346700eaa795fa5679771160ac693d1b53116
Fernando Ellery <fernando.hle@gmail.com>
2024-07-19 18:46:09 -0300

    POST e DELETE de favoritos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -2,6 +2,7 @@ import Input from '../Input'
 import styled from 'styled-components'
 import { useEffect, useState } from 'react'
 import { getLivros } from '../servicos/livros'
+import { postFavorito } from '../servicos/favoritos'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -55,6 +56,11 @@ function Pesquisa() {
         setLivros(livrosDaAPI)
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id)
+        alert(`Livro de id:${id} inserido!`)
+}
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -68,8 +74,8 @@ function Pesquisa() {
                 }}
             />
             { livrosPesquisados.map( livro => (
-                <Resultado>
-                    <img src={livro.src} alt={livro.nome}/>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
+                    <img src={livro.src} alt=''/>
                     <p>{livro.nome}</p>
                 </Resultado>
             )) }
